9f16b9187ba0449a104f316e250cead952ae08f8
FakeMonkey <msolwooden@outlook.com>
2023-05-07 22:23:28 +0800

    构建后安装的指令用专门的构建脚本控制。 关闭文档和测试的生成。

--- install_release_x64.bat
+++ install_release_x64.bat
@@ -1,6 +1,7 @@
 mkdir build
 cmake -S . -B build -A x64 -D CMAKE_BUILD_TYPE=Release ^
-    -D CMAKE_INSTALL_PREFIX:PATH="%~dp0../../../3rd_party_libraries/FMT"
+    -D CMAKE_INSTALL_PREFIX:PATH="%~dp0../../../3rd_party_libraries/FMT" ^
+    -D FMT_TEST:BOOL="0" -D FMT_DOC:BOOL="0"
 cmake --build build --config Release
 cmake --install build --config Release
 pause
